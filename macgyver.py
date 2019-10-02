227fba7b68c105f2d7930cb33c050ff728372eaa
Fabrice Pringuet <fabrice.pringuet@free.fr>
2019-10-02 11:39:33 +0200

    macgyver module updated

--- macgyver.py
+++ macgyver.py
@@ -1,30 +1,34 @@
-import constants
+from constants import *
+from map import *
 
-class MacGyver:    #Class who manage main character movements on the maze
+class MG:    #Class who manage main character movements on the maze
 
-	def __init__(self, )
+	def __init__(self, image):
 		# define the initial position of MacGyver
+		self.image = image
 		self.position_x = 0
 		self.position_y = 0
-		self.position_case_x = 0
-		self.position_case_y = 0
 
 	def move_right(self):
-		if self.position_x < (constants.COLUMNS - 1):
-			self.position_x += 1
-			self.position_case_x = (self.position_x * constants.SPRITE_SIZE)
+		if self.position_x < (COLUMNS - 1):
+			if self.position_x + 1 != self.pos_walls:
+				self.position_x += 1
+				self.position_case_x = (self.position_x * SPRITE_SIZE)
 
 	def move_left(self):
 		if self.position_x > 0 :
-			self.position_x -= 1
-			self.position_case_x = (self.position_x * constants.SPRITE_SIZE)
+			if self.position_x - 1 != self.pos_walls:
+				self.position_x -= 1
+				self.position_case_x = (self.position_x * SPRITE_SIZE)
 
-	def move_top(self):
+	def move_up(self):
 		if self.position_y > 0 :
-			self.position_y -= 1
-			self.position_case_y = (self.position_y * constants.SPRITE_SIZE)
+			if self.position_y - 1 != self.pos_walls:
+				self.position_y -= 1
+				self.position_case_y = (self.position_y * SPRITE_SIZE)
 
-	def move_bottom(self):
-		if self.position_y < (constants.ROWS - 1):
-			self.position_y += 1
-			self.position_case_y = (self.position_y * constants.SPRITE_SIZE)
+	def move_down(self):
+		if self.position_y < (ROWS - 1):
+			if self.position_y + 1 != self.pos_walls:
+				self.position_y += 1
+				self.position_case_y = (self.position_y * SPRITE_SIZE)
